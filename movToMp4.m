605efb2fd84833b91101e98f03e6f32363567824
ocarlott <ocarlott@users.noreply.github.com>
2020-05-29 13:21:07 -0500

    Update movToMp4.m

--- movToMp4.m
+++ movToMp4.m
@@ -21,8 +21,6 @@ RCT_EXPORT_METHOD(convertMovToMp4: (NSString*)filename
     AVURLAsset *avAsset = [AVURLAsset URLAssetWithURL:urlFile options:nil];
 
     NSArray *compatiblePresets = [AVAssetExportSession exportPresetsCompatibleWithAsset:avAsset];
-
-    AVAssetExportSession *exportSession = [[AVAssetExportSession alloc] initWithAsset:avAsset presetName:AVAssetExportPresetMediumQuality];
     AVAssetExportSession *exportSession = [[AVAssetExportSession alloc] initWithAsset:avAsset presetName:AVAssetExportPresetHighestQuality];
     NSFileManager *manager = [NSFileManager defaultManager];
     NSError *error = nil;
